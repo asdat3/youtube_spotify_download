b7c581ec5e150d0b4d035f5dd84e91db53c66a7d
ninjas3 <nina.wunder@icloud.com>
2022-08-22 18:32:59 +0200

    as def

--- config/Youtube-Spotify-Downloader.py
+++ config/Youtube-Spotify-Downloader.py
@@ -3,6 +3,8 @@ from pytube import YouTube
 import youtube_dl
 from pytube import Playlist
 import moviepy.editor as mp 
+from typer import Typer
+
 
 #------------------------------
 # Youtube-Spotify-Downloader
@@ -34,25 +36,32 @@ import moviepy.editor as mp
 #     new_file.write_audiofile(mp3_path)
 #     os.remove(mp4_path)
 
-playlist = Playlist("https://www.youtube.com/playlist?list=PL4Qsk8HhL0PtunPc8jEADP36ndjdmlwQE")
 
-#prints each video url, which is the same as iterating through playlist.video_urls
-for url in playlist:
-    print(url)
-#prints address of each YouTube object in the playlist
-for vid in playlist.videos:
-    print(vid)
-for url in playlist:
-    YouTube(url).streams.filter(only_audio=True).first().download("./test")
-# for url in playlist:
-#     YouTube(url).streams.first().download("./test")
+def download(url: str, outpath: str = "./"):
+    playlist = Playlist(url)
 
-folder = "./test"
+    #prints each video url, which is the same as iterating through playlist.video_urls
+    for url in playlist:
+        print(url)
+    #prints address of each YouTube object in the playlist
+    for vid in playlist.videos:
+        print(vid)
+    for url in playlist:
+        YouTube(url).streams.filter(only_audio=True).first().download(outpath)
+    # for url in playlist:
+    #     YouTube(url).streams.first().download("./test")
 
-for file in os.listdir(folder):
-  if re.search('mp4', file):
-    mp4_path = os.path.join(folder,file)
-    mp3_path = os.path.join(folder,os.path.splitext(file)[0]+'.mp3')
-    new_file = mp.AudioFileClip(mp4_path)
-    new_file.write_audiofile(mp3_path)
-    os.remove(mp4_path)
+    folder = "./test"
+
+    for file in os.listdir(folder):
+        if re.search('mp4', file):
+            mp4_path = os.path.join(folder,file)
+            mp3_path = os.path.join(folder,os.path.splitext(file)[0]+'.mp3')
+            new_file = mp.AudioFileClip(mp4_path)
+            new_file.write_audiofile(mp3_path)
+            os.remove(mp4_path)
+
+download(
+    "https://www.youtube.com/playlist?list=PL4Qsk8HhL0PtunPc8jEADP36ndjdmlwQE",
+    "./test"
+)
